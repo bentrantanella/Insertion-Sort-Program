bc67d85f12e6f3c9bb04f3a1705566c908247b4d
20btrantanella <20btrantanella@WA-248-9579.schools.westford-ma.gov>
2020-01-30 08:27:17 -0500

    1/30

---
+++ Insertion Sort Program/src/InputDialog.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import BreezySwing.*;
+import java.util.*;
+
+public class InputDialog extends GBDialog {
+	
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+	
+	public InputDialog(JFrame parent, ArrayList<Integer> nums) {
+		super(parent);
+		setTitle("Input");
+		setSize(300, 300);
+		
+		numbers = nums;
+		
+	}
+	
+	JLabel addLabel = addLabel("Add number to list",1,1,1,1);
+	IntegerField addField = addIntegerField(0,1,2,1,1);
+	JButton addButton = addButton("Add",2,1,1,1);
+	
+	
+	
+	public void ButtonClicked(JButton button) {
+		
+	}
+}
---
+++ Insertion Sort Program/src/InsertionSortGUI.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import BreezySwing.*;
+import java.util.*;
+
+public class InsertionSortGUI extends GBFrame {
+	
+	public static void main(String[] args) {
+		JFrame frm = new InsertionSortGUI();
+		frm.setTitle("Mean, median, and mode");
+		frm.setSize(300, 300);
+		frm.setVisible(true);
+		
+	}
+
+	JButton inputButton = addButton("Input",1,1,1,1);
+	JButton meanButton = addButton("Find mean",2,1,1,1);
+	JButton modeButton = addButton("Find mode",3,1,1,1);
+	JButton medianButton = addButton("Find median",4,1,1,1);
+	JButton printAllButton = addButton("Print all",5,1,1,1);
+	JButton resetButton = addButton("Reset",6,1,1,1);
+	
+	public InsertionSortGUI() {
+		meanButton.setEnabled(false);
+		modeButton.setEnabled(false);
+		medianButton.setEnabled(false);
+	}
+	
+	public void ButtonClicked(JButton button) {
+		if (button == inputButton) {
+			
+		}
+		
+		if (button == meanButton) {
+			
+		}
+		
+		if (button == modeButton) {
+			
+		}
+		
+		if (button == medianButton) {
+			
+		}
+		
+		if (button == printAllButton) {
+			
+		}
+		
+		if (button == resetButton) {
+			
+		}
+	}
+}
